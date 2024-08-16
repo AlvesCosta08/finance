7da874f0b658595081f42a82aee0644e7c2664ca
Alves <alvescosta74@gmail.com>
2024-08-16 11:51:48 -0300

    criado psql

---
+++ db/query/account.sql
@@ -0,0 +1,56 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+    user_id,
+    category_id,
+    title,
+    type,
+    description,
+    value,
+    date
+) VALUES (
+    $1, $2, $3, $4, $5, $6, $7
+) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * 
+FROM accounts 
+WHERE id = $1 
+LIMIT 1;
+
+-- name: GettAccount :many
+SELECT
+a.id,
+a.user_id,
+a.title,
+a.type,
+a.description,
+a.value,
+a.date,
+a.created_at,
+c.title as catgory_title
+FROM accounts a 
+LEFT JOIN categories c ON c.id = a.category_id
+WHERE a.user_id = $1 and a.type = $2
+and a.category_id = $3 and a.title like $4
+and a.description like $5 and a.date = $6;
+;
+
+-- name: GetAccountReports :one
+SELECT SUM(value) AS sum_value FROM accounts 
+where user_id = $1 and type = $2;
+
+-- name: GetAccountGrafic :one
+SELECT COUNT(*) FROM accounts 
+where user_id = $1 and type = $2;
+
+
+-- name: UpdateAccount :one
+UPDATE accounts 
+SET title = $2, description = $3, value = $4
+WHERE id = $1
+RETURNING *;
+
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
